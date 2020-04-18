d73796774d7be92c45b430aaf1c16b61cfc64b7a
nawaazwill11 <mastermindjim@gmail.com>
2020-04-18 10:27:46 +0530

    dummy database test with note editor

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,3 +1,7 @@
+const database = {
+    notes: []
+}
+
 const notes = document.querySelectorAll('.note');
 notes.forEach((note) => {
     note.addEventListener('click', function (e) {
@@ -44,6 +48,11 @@ notes.forEach((note) => {
         menu_options.classList.add('visible');
     })
 });
+const edited_note = {
+    id: 1,
+    title: '',
+    data: []
+};
 
 const editor_closer = document.querySelector('#editor-close');
 editor_closer.onclick = function (e) {
@@ -52,44 +61,62 @@ editor_closer.onclick = function (e) {
 }
 
 const kp_block = {
-    html: `<div class="kp-block">
-    <div class="kp-block-content">
-      <div class="kp-block-layer">
-        <div class="kp-keypoint">
-          <input class="inp-flat" type="text" placeholder="Keypoint">
+    html: function (id) {
+
+    return (`<div class="kp-block" data-id=${id}>
+        <div class="kp-block-content">
+        <div class="kp-block-layer">
+            <div class="kp-keypoint">
+            <input class="inp-flat" type="text" placeholder="Keypoint">
+            </div>
         </div>
-      </div>
-      <div class="kp-block-layer">
-        <div class="description">
-          <input class="inp-flat" type="text" placeholder="Description">
+        <div class="kp-block-layer">
+            <div class="description">
+            <input class="inp-flat" type="text" placeholder="Description">
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="kp-block-action-container">
-      <div class="kp-block-action">
-        <div class="action-node remove">
-          <button>-</button>
         </div>
-        <div class="action-node copy">
-          <button>C</button>
+        <div class="kp-block-action-container">
+        <div class="kp-block-action">
+            <div class="action-node remove">
+            <button>-</button>
+            </div>
+            <div class="action-node copy">
+            <button>C</button>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>`,
+        </div>
+    </div>`)
+    },
   init: kpBlockInit
 }
+
+let kp_id_seq = 0;
+
 const kp_adder = document.querySelector('#kp-adder button');
 kp_adder.onclick = function(e) {
-    $('#kp-main').append(kp_block.html);
+    const id = edited_note.id + '_kp_' + ++kp_id_seq; 
+    edited_note.data.push({
+        id: id,
+        keypoint: '',
+        desc: ''
+    });
+    $('#kp-main').append(kp_block.html(id));
+    console.log(edited_note);
     kpBlockInit();
 };
 
 function kpBlockInit() {
-    const kp_block_list = document.querySelectorAll('.kp-block-action');
-    kp_block_list.forEach((block) => {
+    const kp_block_action_list = document.querySelectorAll('.kp-block-action');
+    kp_block_action_list.forEach((block) => {
         const remove = block.querySelector('.remove button');
         remove.onclick =  function (e) {
-            this.closest('.kp-block').remove();
+            const _block = this.closest('.kp-block');
+            const block_id = _block.id;
+            const index = edited_note.data.findIndex((kp) => kp.id === block_id);
+            edited_note.data.splice(index, 1);
+            console.log(edited_note);
+            _block.remove();
         };
 
         const copy = block.querySelector('.copy button');
@@ -97,6 +124,7 @@ function kpBlockInit() {
             const clone = this.closest('.kp-block').cloneNode(true);
             document.querySelector('#kp-main').appendChild(clone);
             kp_block.init();
-        }
+        };
+
     });
 }
